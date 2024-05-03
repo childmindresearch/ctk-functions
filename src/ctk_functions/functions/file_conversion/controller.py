3bdbed01891647b1e80f985b44da40bc3d71ae62
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-03 16:43:40 -0400

    Make warnings red (#21)

--- src/ctk_functions/functions/file_conversion/controller.py
+++ src/ctk_functions/functions/file_conversion/controller.py
@@ -1,7 +1,10 @@
 """Functions for converting files between different formats."""
 
+import re
 import tempfile
 
+import cmi_docx
+import docx
 import pypandoc
 
 from ctk_functions.text import corrections
@@ -33,4 +36,23 @@ def markdown2docx(
             outputfile=temp_file.name,
         )
         temp_file.seek(0)
+        mark_warnings_as_red(temp_file.name)
         return temp_file.read()
+
+
+def mark_warnings_as_red(docx_file: str) -> None:
+    """Marks warning templates as red.
+
+    We use {{!WARNING-TEXT}} as a template for warnings that should be marked red.
+
+    Args:
+        docx_file: The .docx file.
+    """
+    document = docx.Document(docx_file)
+    extend_document = cmi_docx.ExtendDocument(document)
+    text = "\n".join([paragraph.text for paragraph in document.paragraphs])
+    warningRegex = re.compile(r"{{!.*?}}")
+    matches = set(warningRegex.finditer(text))
+    for match in matches:
+        extend_document.replace(match.group(), match.group(), {"font_rgb": (255, 0, 0)})
+    document.save(docx_file)
