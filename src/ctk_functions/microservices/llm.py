b6acf502067ab0438ba5ce19c7c628add77be250
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-10 17:21:25 -0400

    Add LLM logging

--- src/ctk_functions/microservices/llm.py
+++ src/ctk_functions/microservices/llm.py
@@ -2,9 +2,12 @@
 
 from typing import Literal
 
+from ctk_functions import config
 from ctk_functions.microservices import aws, azure, utils
 
 VALID_LLM_MODELS = Literal["gpt-4o", "anthropic.claude-3-opus-20240229-v1:0"]
+
+logger = config.get_logger()
 
 
 class LargeLanguageModel(utils.LlmAbstractBaseClass):
@@ -22,8 +25,10 @@ class LargeLanguageModel(utils.LlmAbstractBaseClass):
         """
         self.client: azure.AzureLlm | aws.ClaudeLlm
         if model == "gpt-4o":
+            logger.info("Using Azure LLM")
             self.client = azure.AzureLlm()
         elif model == "anthropic.claude-3-opus-20240229-v1:0":
+            logger.info("Using AWS LLM")
             self.client = aws.ClaudeLlm()
         else:
             raise ValueError(f"Invalid LLM model: {model}")
