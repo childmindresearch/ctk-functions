e12194afde4c53a6ac28b3b3f7e3f44ccb4133d7
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-16 12:10:18 -0400

    feat: improve languagetool speed by only checking required rules

--- src/ctk_functions/functions/intake/utils/language_utils.py
+++ src/ctk_functions/functions/intake/utils/language_utils.py
@@ -1,6 +1,6 @@
 """Utilities for correcting grammar and syntax."""
 
-from typing import Sequence
+import concurrent.futures
 
 import cmi_docx
 import docx
@@ -16,7 +16,7 @@ NLP = spacy.load("en_core_web_sm")
 # used for the dynamic text. Rules applying only to static template text
 # should be fixed in the template itself.
 
-DEFAULT_LANGUAGE_RULES = (
+DEFAULT_LANGUAGE_RULES = {
     "BASE_FORM",
     "CONSECUTIVE_SPACES",
     "PERS_PRONOUN_AGREEMENT",
@@ -24,9 +24,7 @@ DEFAULT_LANGUAGE_RULES = (
     "THE_US",
     "UPPERCASE_SENTENCE_START",
     "WEEK_HYPHEN",
-)
-
-ruleIds: set[str] = set()
+}
 
 
 class DocumentCorrections:
@@ -35,8 +33,7 @@ class DocumentCorrections:
     def __init__(
         self,
         document: document.Document,
-        enabled_rules: Sequence[str] | None = DEFAULT_LANGUAGE_RULES,
-        disabled_rules: Sequence[str] | None = None,
+        enabled_rules: set[str] = DEFAULT_LANGUAGE_RULES,
     ) -> None:
         """Initializes the corrector with a document.
 
@@ -44,19 +41,21 @@ class DocumentCorrections:
             document: The docx document to correct.
             enabled_rules: The rules to enable for the correction. If None, all rules
                 are enabled.
-            disabled_rules: The rules to disable for the correction. If None, no rules
-                are disabled.
 
         """
         self.document = document
-        self.correcter = corrections.LanguageCorrecter()
-        self.enabled_rules = set(enabled_rules) if enabled_rules else set()
-        self.disabled_rules = set(disabled_rules) if disabled_rules else set()
+        self.correcter = corrections.LanguageCorrecter(enabled_rules)
 
     def correct(self) -> None:
         """Makes corrections based on the enabled and disabled rules."""
-        for paragraph in self.document.paragraphs:
-            self._correct_paragraph(paragraph)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self._correct_paragraph, paragraph)
+                for paragraph in self.document.paragraphs
+            ]
+
+            for _ in concurrent.futures.as_completed(futures):
+                pass
 
     def _correct_paragraph(self, paragraph: docx.text.paragraph.Paragraph) -> None:
         """Corrects conjugations in a single paragraph.
@@ -65,14 +64,7 @@ class DocumentCorrections:
             paragraph: The paragraph to correct.
         """
         sentences = list(NLP(paragraph.text).sents)
-        new_sentences = [
-            self.correcter.run(
-                sentence.text,
-                enabled_rules=self.enabled_rules,
-                disabled_rules=self.disabled_rules,
-            )
-            for sentence in sentences
-        ]
+        new_sentences = [self.correcter.run(sentence.text) for sentence in sentences]
         extended_pargraph = cmi_docx.ExtendParagraph(paragraph)
         for old, new in zip(sentences, new_sentences):
             if old.text != new:
