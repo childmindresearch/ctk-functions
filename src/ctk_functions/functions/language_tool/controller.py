5a0d713b8d15004db4b2261dbfe6b094ef3563fa
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-22 13:55:42 -0400

    feat: add language tool endpoint

---
+++ src/ctk_functions/functions/language_tool/controller.py
@@ -0,0 +1,17 @@
+"""Functions for converting files between different formats."""
+
+from ctk_functions.text import corrections
+
+
+def language_tool(text: str, rules: list[str] = []) -> str:
+    """Corrects the grammar of the input text.
+
+    Args:
+        text: The text to correct.
+        rules: The rules to enable.
+
+    Returns:
+        The corrected text.
+    """
+    correcter = corrections.LanguageCorrecter(rules)
+    return correcter.run(text)
