6f23164411d3d7aa9cffd0146b10e7cbda87f200
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-25 14:18:56 -0400

    doc: add additional documentatoin

--- src/ctk_functions/functions/file_conversion/lua/tab.lua
+++ src/ctk_functions/functions/file_conversion/lua/tab.lua
@@ -1,4 +1,6 @@
 -- Provides tab support for markdown to docx conversions.
+-- Pandoc automatically converts all consecutives tabs and spaces to a single space,
+-- so we need to rely on a custom charater (^t) to represent a tab in the markdown file.
 
 return {
     {
