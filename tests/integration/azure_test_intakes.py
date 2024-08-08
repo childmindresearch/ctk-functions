e7de6f3f6cd2eab6a0b2e8c6c2c00ae6e066f58a
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-08-08 10:04:14 -0400

    feat: test across 3 surveys

---
+++ tests/integration/azure_test_intakes.py
@@ -0,0 +1,19 @@
+"""Test a few survey IDs."""
+
+import asyncio
+import os
+
+from ctk_functions.functions.intake import controller as intake_controller
+from ctk_functions.text import corrections
+
+corrections.LanguageCorrecter(enabled_rules=set())  # Download LanguageTool
+survey_ids = [s.strip() for s in os.environ["SURVEY_IDS"].split(",")]
+
+promises = [
+    intake_controller.get_intake_report(
+        survey_id, "anthropic.claude-3-5-sonnet-20240620-v1:0"
+    )
+    for survey_id in survey_ids
+]
+
+asyncio.get_event_loop().run_until_complete(asyncio.gather(*promises))
