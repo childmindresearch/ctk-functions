c63fdfa76e9e6d98d413fbf83fe08f6d6a30ab43
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-12-27 10:11:48 -0500

    hotfix: Azure functions referencing module, not fastapi app.

--- function_app.py
+++ function_app.py
@@ -4,4 +4,4 @@ import azure.functions as func
 
 from ctk_functions import app as fastapi_app
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
+app = func.AsgiFunctionApp(app=fastapi_app.app, http_auth_level=func.AuthLevel.FUNCTION)
