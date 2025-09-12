75088921d391e7606392e441c29ea985f3d99196
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-09-12 10:20:28 -0400

    Prod model to use markdown syntax less (#240)
    Signed-off-by: reinder.vosdewael@childmind.org <reinder.vosdewael@childmind.org>

--- src/ctk_functions/routers/intake/intake_processing/writer_llm.py
+++ src/ctk_functions/routers/intake/intake_processing/writer_llm.py
@@ -84,6 +84,7 @@ requirements:
 - If acronyms are used, maintain the acronyms; do not guess at the meaning of the acronym.
 - Do not make subjective judgements on whether something was e.g. easy or challenging unless these are explicitly mentioned in the source material.
 - Always refer to the child and parent by name, i.e. do not use phrases like "the patient".
+- DO NOT USE MARKDOWN SYNTAX, i.e. do not use headers with #, do not use lists with -, or quotes with >, or any similar syntax. All text will be placed directly into a Word document as plain text and as such, will be directly integrated with its surroundings.
 """  # noqa: E501
 
 
